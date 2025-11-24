84c3c180d1cb4e43261ba2b48b5b745fa7570270
MiguelUrquiza <miguelurquiza.contacto@gmail.com>
2025-11-24 18:55:35 +0100

    Initial Commit

---
+++ ApiRecetasMiguelUrquiza/src/main/java/com/salesianostriana/dam/ApiRecetasMiguelUrquiza/ApiRecetasMiguelUrquizaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ApiRecetasMiguelUrquiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRecetasMiguelUrquizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRecetasMiguelUrquizaApplication.class, args);
+	}
+
+}
